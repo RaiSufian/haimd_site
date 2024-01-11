84dd786f4f5002df036e30afaae0cd858bca28cb
RaiSufian <mashancs571@gmail.com>
2024-01-11 14:39:58 +0500

    update exp list

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -9,18 +9,18 @@ const Experience = () => {
                 <div className="font-inter work_experience relative before:content-[''] before:bg-gray-800 before:text-4xl before:absolute before:w-1 before:h-[100%] before:right-[50%] before:rounded-xl pt-4 before:translate-x-2/4">
                     <div className="exper_items py-7 w-[50%] md:pr-7 pr-3 relative before:absolute before:content-[''] before:h-4 before:w-4 before:bg-themeColor before:right-0 before:rotate-45 before:translate-x-1/2 before:translate-y-3/4">
                         <h5 className="md:text-2xl text-md font-extrabold">React JS Developer</h5>
-                        <p className="flex items-center justify-between text-sm md:text-md"><a href="https://storakdigital.com/">Storak Digital</a><span>3/2022 - Present</span></p>
+                        <p className="flex items-center justify-between text-sm md:text-md"><a href="https://storakdigital.com/">Storak Digital</a><span>Apr 2022 - Sep 2023</span></p>
                     </div>
                     <div className="flex justify-end">
 
                         <div className="exper_items py-7 w-[50%] md:pl-7 pl-3 relative before:absolute before:content-[''] before:h-4 before:w-4 before:bg-themeColor before:left-0 before:rotate-45 before:-translate-x-1/2 before:translate-y-3/4">
                             <h5 className="md:text-2xl text-md font-extrabold">React JS Developer</h5>
-                            <p className="flex items-center justify-between text-sm md:text-md"><a href="https://www.linkedin.com/company/kings-digital-solution-pvt-ltd/?originalSubdomain=pk">King Digital Solutions</a><span>5/2021 - 3/2022</span></p>
+                            <p className="flex items-center justify-between text-sm md:text-md"><a href="https://www.linkedin.com/company/kings-digital-solution-pvt-ltd/?originalSubdomain=pk">King Digital Solutions</a><span>May 2021 - Mar 2022</span></p>
                         </div>
                     </div>
                     <div className="exper_items py-7 w-[50%] md:pr-7 pr-3 relative before:absolute before:content-[''] before:h-4 before:w-4 before:bg-themeColor before:right-0 before:rotate-45 before:translate-x-1/2 before:translate-y-3/4">
                         <h5 className="md:text-2xl text-md font-extrabold">Front end Developer</h5>
-                        <p className="flex items-center justify-between text-sm md:text-md"><a href="https://www.softileo.com/">Softileo </a><span>3/2019 - 4/2020</span></p>
+                        <p className="flex items-center justify-between text-sm md:text-md"><a href="https://www.softileo.com/">Next Pedia</a><span>Aug 2019 - Apr 2021</span></p>
                     </div>
                 </div>
             </div>
